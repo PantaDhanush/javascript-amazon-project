65badaff4cd2d2fd1f62e95f9c685648723088c1
Dhanush <layola.psreddy@gmail.com>
2025-12-06 18:03:20 +0530

    Making Cart Section interactive

--- Scripts/Amazon.js
+++ Scripts/Amazon.js
@@ -46,7 +46,7 @@ products.forEach((product)=>{
           </div>
 
           <button class="add-to-cart-button button-primary js-add-to-cart"
-            data-product-name="${product.name}">
+            data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -59,15 +59,24 @@ document.querySelectorAll('.js-add-to-cart')
     .forEach((button,index)=>{
         button.addEventListener('click',()=>{
             // console.log(button.dataset)
-            const productName=button.dataset.productName;
-
-            
-            cart.push(
-                {
-                    productName:productName,
-                    quantity:1
+            const productId=button.dataset.productId;
+            let matchingItem;
+            cart.forEach((item)=>{
+                if(productId===item.productId){
+                    matchingItem=item;
+                    return;
                 }
-            );
+            });
+            if(matchingItem){
+                matchingItem.quantity++;
+            }else{
+                cart.push(
+                    {
+                        productId:productId,
+                        quantity:1
+                    }
+                )
+            }
             console.log(cart)
         })
     })
