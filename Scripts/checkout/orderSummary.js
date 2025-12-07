42e054acd5c7512a5c549905a24df762f18f3319
Dhanush <layola.psreddy@gmail.com>
2025-12-07 20:58:52 +0530

    Make Payment Summary Interactive

--- Scripts/checkout/orderSummary.js
+++ Scripts/checkout/orderSummary.js
@@ -4,6 +4,7 @@ import formatCurrency  from "../Utils/Money.js";
 import { totalItemsInCart } from "../Utils/ItemsInCart.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import { deliveryOptions,getDeliveryOption } from "../../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary(){
     let totalCart='';
@@ -104,6 +105,7 @@ export function renderOrderSummary(){
             const container=document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove();
             renderOrderSummary();
+            renderPaymentSummary();
         })
     })
 
@@ -131,6 +133,7 @@ export function renderOrderSummary(){
             updateQuantity(productId,newQuantity);
             container.classList.remove('is-editing-quantity');
             renderOrderSummary();
+            renderPaymentSummary();
         });
     });
 
@@ -142,6 +145,7 @@ export function renderOrderSummary(){
             const {productId,deliveryOptionId}=element.dataset;
             updateDeliveryOption(productId,deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         })
     })
 }
