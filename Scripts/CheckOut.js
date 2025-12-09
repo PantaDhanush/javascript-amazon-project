1a4ea187930bbff89810b875b7b9717eaca8074f
Dhanush <layola.psreddy@gmail.com>
2025-12-09 18:28:19 +0530

    Refactor code structure for improved readability and maintainability

--- Scripts/CheckOut.js
+++ Scripts/CheckOut.js
@@ -1,5 +1,7 @@
+import { loadProducts } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import "../data/backend-practice.js";
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+});
