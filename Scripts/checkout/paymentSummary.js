ee7d76247cb782fb23941b3478dba8c3b1e0b2ec
Dhanush <layola.psreddy@gmail.com>
2025-12-09 21:11:47 +0530

    Create Order using the Backend

--- Scripts/checkout/paymentSummary.js
+++ Scripts/checkout/paymentSummary.js
@@ -1,7 +1,9 @@
 import { cart } from "../../data/cart.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
+import { addOrder } from "../../data/Orders.js";
 import { getProduct } from "../../data/products.js";
 import formatCurrency from "../Utils/Money.js";
+
 export function renderPaymentSummary(){
     let productPriceCents=0;
     let shippingPriceCents=0;
@@ -46,9 +48,28 @@ export function renderPaymentSummary(){
         <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
         </div>
 
-        <button class="place-order-button button-primary">
-        Place your order
+        <button class="place-order-button button-primary js-place-order">
+            Place your order
         </button>
     `;
     document.querySelector('.js-payment-summary').innerHTML=paymentSummaryHTML;
+    document.querySelector('.js-place-order')
+        .addEventListener("click",async()=>{
+            try{
+                const response=await fetch('https://supersimplebackend.dev/orders',{
+                    method:'POST',
+                    headers:{
+                        'Content-Type':'application/json'
+                    },
+                    body:JSON.stringify({
+                        cart:cart
+                    })
+                });
+                const order=await response.json();
+                addOrder(order);
+            }catch(error){
+                console.log("Unexcepted Error!! Try again Later");
+            }
+            window.location.href="orders.html";
+    })
 }
